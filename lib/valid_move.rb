e84a60bb5867ceb55ec51146bbd8c1546f37e2ee
Marc Mills <mdmills1990@gmail.com>
2018-08-01 18:35:37 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,5 +5,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-board[index].between(0,8) && !position_taken?(board, index)
+board[index].between(0,8) && !position_taken?(board,index)
 end
